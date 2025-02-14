9debd3cbdf54b49909166a7d7c353617f32f25d0
misha <mikhail.valiev7@gmail.com>
2025-02-15 00:52:16 +0300

    update .gitlab-ci.yml file

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,14 +5,5 @@ from app.src.main import startup
 
 @pytest.fixture(scope="session", autouse=True)
 def load_model():
-    current_directory = os.getcwd()
-    print("---------------")
-    print(f"Текущая директория: {current_directory}")
-
-    # Список файлов и папок в текущей директории
-    files_and_folders = os.listdir(current_directory)
-    print("Файлы и папки в текущей директории:")
-    for item in files_and_folders:
-        print(item)
     startup()
     yield
